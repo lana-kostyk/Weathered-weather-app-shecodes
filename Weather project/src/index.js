0ee24d501fcad7ca227b824f71f13d06f36ed726
lana-kostyk <70900357+lana-kostyk@users.noreply.github.com>
2020-10-11 17:15:40 +0200

    Added project HTML, CSS and JS files

---
+++ Weather project/src/index.js
@@ -0,0 +1,101 @@
+// Display the time
+let today = new Date();
+
+function now() {
+  let currentTime = document.querySelector("#current-time");
+
+  let hour = today.getHours();
+  let minutes = today.getMinutes();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let time = `${hour}:${minutes}`;
+  currentTime.innerHTML = time;
+}
+setInterval(now, 1000);
+
+// Display the city name from the form input + change the temperature
+function search(event) {
+  event.preventDefault();
+  let searchCityInput = document.querySelector("#search-city-input");
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = searchCityInput.value;
+
+  searchCity(searchCityInput.value);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+// Weather API
+function searchCity(city) {
+  let apiKey = "b4d8edf093296b9aa93443470450c3f6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature-now");
+  temperatureElement.innerHTML = temperature;
+}
+
+// Display day of the week, date, month, year (Sunday 6 September 2020)
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[today.getDay()];
+
+let date = today.getDate();
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[today.getMonth()];
+
+let year = today.getFullYear();
+
+let currentDayDateMonthYear = document.querySelector("h3");
+currentDayDateMonthYear.innerHTML = `${day} ${date} ${month} ${year}`;
+
+// Display converted temperature
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature-now");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature-now");
+  temperatureElement.innerHTML = 19;
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
