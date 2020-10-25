4a48bfda30c89322276d79ba5a2084307ea524c3
lana-kostyk <70900357+lana-kostyk@users.noreply.github.com>
2020-10-25 16:26:23 +0100

    Added search engine to display current weather information

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
+let today = new Date();
+
+function now() {
+  let currentTime = document.querySelector("#current-time");
+
+  let hour = today.getHours();
+  let minutes = today.getMinutes();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let time = `${hour}:${minutes}`;
+  currentTime.innerHTML = time;
+}
+setInterval(now, 1000);
+
+function date() {
+     let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[today.getDay()];
+
+let date = today.getDate();
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[today.getMonth()];
+
+let year = today.getFullYear();
+
+let currentDate = document.querySelector("#current-info");
+    currentDate.innerHTML = `${day} ${date} ${month} ${year}`;
+    
+} 
+date();
+
+
 function displayTemperature(response) {
     console.log(response.data);  
     let temperatureElement = document.querySelector("#temperature-now");
@@ -5,20 +62,40 @@ function displayTemperature(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
-    let feelslikeElement = document.querySelector ("#feels-like")
+    let feelslikeElement = document.querySelector("#feels-like");
+    let iconElement = document.querySelector("#icon");
+    
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
-  feelslikeElement.innerHTML = Math.round(response.data.main.feels_like);
+    feelslikeElement.innerHTML = Math.round(response.data.main.feels_like);
+    iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+    
+}
+function searchCity(city) {
+  let apiKey = "b4d8edf093296b9aa93443470450c3f6";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
 }
 
+function search(event) {
+  event.preventDefault();
+  let searchCityInput = document.querySelector("#search-city-input");
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = searchCityInput.value;
 
-let apiKey = "b4d8edf093296b9aa93443470450c3f6";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Krakow&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+  searchCity(searchCityInput.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+
 
 
