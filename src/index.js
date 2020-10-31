121a882e3757eb43903fa6c077ba16e9a658cbea
lana-kostyk <70900357+lana-kostyk@users.noreply.github.com>
2020-10-31 16:48:59 +0100

    adding class="embed-responsive-item" to image to adapt to mobile version

--- src/index.js
+++ src/index.js
@@ -107,7 +107,7 @@ function dispalyForecast(response) {
       <div>
         ${formatHours(forecast.dt * 1000)}
       </div>
-      <div class="d-flex p-2 bd-highlight"><img
+      <div><img class="embed-responsive-item"
         src="http://openweathermap.org/img/wn/${
           forecast.weather[0].icon
         }@2x.png"/></div>
